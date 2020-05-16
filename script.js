1c1d2d2a09de810adae662837595f26a4fe77de3
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-05-16 19:19:00 -0400

    progress, but not mission success

--- script.js
+++ script.js
@@ -10,4 +10,54 @@
    <li>Number of Moons: ${}</li>
 </ol>
 <img src="${}">
+
+
+The pilot and co-pilot names should be strings and the fuel level and cargo mass should be numbers.
 */
+document.addEventListener("DOMContentLoaded", function() {
+   const pilotName = document.getElementById("pilotName");
+   const coPilotName = document.getElementById("copilotName");
+   const fuelLevel = document.getElementById("fuelLevel");
+   const cargoMass = document.getElementById("cargoMass");
+   const buttonPress = document.getElementById("launchForm");
+
+
+   buttonPress.addEventListener("submit", function() {
+      let alertRequiredChanges = false;
+      let alertMessage = "Unable to process your information. \n";
+      if ((pilotName.value === "") || (coPilotName.value === "") || (fuelLevel.value === "") || (cargoMass.value === "")) {
+         event.preventDefault();
+      }
+      if ((isNaN(pilotName.value) === true) && (isNaN(coPilotName.value) === true) && (isNaN(fuelLevel.value) === false) && (isNaN(cargoMass.value)=== false)) {
+         alert("fuck ywh");
+         
+      } /*if (isNaN(pilotName.value) === false) {
+         alertRequiredChanges = true;
+         alertMessage += "Pilot name needs to be a string of letters, you robot-naming fool... \n";
+      } if (isNaN(coPilotName.value) === false) {
+         alertRequiredChanges = true;
+         alertMessage += "Co-pilot name needs to be a string of letters, you robot-naming fool... \n";
+      } if (isNaN(fuelLevel.value) === true) {
+         alertRequiredChanges = true;
+         alertMessage += "Fuel level name needs to be a string of letters, you robot-naming fool... \n";
+      } if (isNaN(cargoMass.value)=== true) {
+         alertRequiredChanges = true;
+         alertMessage += "Cargo mass name needs to be a string of letters, you robot-naming fool... \n";
+      }*/
+
+
+      if (alertRequiredChanges) {
+         alert(alertMessage);
+      } else {
+         alert("should send info now");
+      }
+      
+      
+   })
+
+
+
+
+
+
+});
